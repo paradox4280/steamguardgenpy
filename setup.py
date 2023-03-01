26fad8c2f650babb73e669b6991afcd2087bca0a
paradox4280 <zmf5c2fi@pm.me>
2023-03-01 05:04:57 +0430

    initial commit

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='steamguardgenpy',
     version=1.0,
     description='Generate steam twofactor (onetime/TOTP) code.',
+    url='https://github.com/paradox4280/steamguardgenpy',
     author='paradox4280',
     license='MIT',
-    packages=['steamguardgenpy'],
-    zip_safe=False
+    packages=find_packages()
 )
